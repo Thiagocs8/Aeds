ce6c0ce8f25d34733eac78b2c217cd9346fbe2bd
Thiago Costa Soares <trigocs64@gmail.com>
2024-06-17 16:59:05 -0300

    Update robô.c

--- maratona/robô.c
+++ maratona/robô.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
 
+int main() {
+    int L, C;
+    int A, B; 
+    int mapa[1000][1000]; 
+
+   
+    printf("Digite as dimensões do salão (L e C): ");
+    scanf("%d %d", &L, &C);
+    
+    
+    printf("Digite a posição inicial do robô (A e B): ");
+    scanf("%d %d", &A, &B);
+    
+    
+    printf("Digite a matriz representando os ladrilhos (1 para preto, 0 para branco):\n");
+    
+    for (int i = 0; i < L; i++) {
+        for (int j = 0; j < C; j++) {
+            scanf("%d", &mapa[i][j]);
+        }
+    }
+    
+    
+    int linha_atual = A - 1; 
+    int coluna_atual = B - 1; 
+    
+    while (coluna_atual < C && mapa[linha_atual][coluna_atual] == 1) {
+        coluna_atual++;
+    }
+    
+    
+    printf("Posição final do robô: %d %d\n", A, coluna_atual + 1); 
+    
+    return 0;
+}
