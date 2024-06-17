7385ef890f3c5e1d79beca56e6f4ad39688a0736
Thiago Costa Soares <trigocs64@gmail.com>
2024-06-17 16:53:34 -0300

    Update torneio de tênis.c

--- maratona/torneio de tênis.c
+++ maratona/torneio de tênis.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+int main() {
+    char resultados[6]; 
+    int vitorias = 0;  
+
+   
+    printf("Digite os resultados dos 6 jogos (V para vitória, P para derrota):\n");
+    for (int i = 0; i < 6; i++) {
+        scanf(" %c", &resultados[i]);
+        if (resultados[i] == 'V') {
+            vitorias++; 
+        }
+    }
+
+   
+     if (vitorias >= 5) {
+        printf("1\n"); 
+    } else if (vitorias >= 3) {
+        printf("2\n"); 
+    } else if (vitorias >= 1) {
+        printf("3\n"); 
+    } else {
+        printf("-1V\n"); 
+    }
+
+    return 0;
+}
